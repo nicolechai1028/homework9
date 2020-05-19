0d58e36f3f47dcf098d65f28da9fc2032f4a4db3
Nicole Chai <nicole@namawell.com>
2020-05-18 19:12:30 -0700

    added post and delete

--- Develop/server.js
+++ Develop/server.js
@@ -3,6 +3,10 @@
 var express = require("express");
 var path = require("path");
 var fs = require("fs");
+var util = require('util');
+
+fs.readFile = util.promisify(fs.readFile);
+fs.writeFile = util.promisify(fs.writeFile);
 
 // Sets up the Express App
 // =============================================================
@@ -13,7 +17,7 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
+app.use(express.static('public'))
 // Routes
 // =============================================================
 
@@ -27,42 +31,71 @@ app.get("/notes", function(req, res) {
   });
 
 
-// Displays all characters
-app.get("/api/characters", function(req, res) {
-  return res.json(characters);
-});
+// API
+app.get('/api/notes', function(req, res) {
+    res.sendFile(path.join(__dirname, 'db/db.json'));
+  });
 
-// Displays a single character, or returns false
-app.get("/api/characters/:character", function(req, res) {
-  var chosen = req.params.character;
-
-  console.log(chosen);
-
-  for (var i = 0; i < characters.length; i++) {
-    if (chosen === characters[i].routeName) {
-      return res.json(characters[i]);
+app.post("/api/notes", async function(req,res) {
+    try{
+    var newNote = req.body;
+    var data = await fs.readFile("db/db.json", "utf8");
+    var notes = JSON.parse(data);
+    notes.push(newNote)
+    await fs.writeFile('db/db.json', JSON.stringify(notes));
+    res.json("ok")
+    }catch(err){
+        console.log(err)
     }
-  }
+})
 
-  return res.json(false);
-});
+app.delete("/api/notes/:name/:id", async (req,res)=>{
+    try{
+        var data = await fs.readFile("db/db.json", "utf8");
+        var notes = JSON.parse(data);
+        notes.splice(req.params.id,1)
+        await fs.writeFile('db/db.json', JSON.stringify(notes));
+        res.json("ok")
+        }catch(err){
+            console.log(err)
+        }
+})
 
-// Create New Characters - takes in JSON input
-app.post("/api/characters", function(req, res) {
-  // req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body parsing middleware
-  var newCharacter = req.body;
 
-  // Using a RegEx Pattern to remove spaces from newCharacter
-  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-  newCharacter.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
+// // Displays a single character, or returns false
+// app.get("/api/characters/:character", function(req, res) {.
+//   var chosen = req.params.character;
 
-  console.log(newCharacter);
+//   console.log(chosen);
 
-  characters.push(newCharacter);
+//   for (var i = 0; i < characters.length; i++) {
+//     if (chosen === characters[i].routeName) {
+//       return res.json(characters[i]);
+//     }
+//   }
 
-  res.json(newCharacter);
-});
+//   return res.json(false);
+// });
+
+// // Create New Characters - takes in JSON input
+// app.post("/api/characters", function(req, res) {
+//   // req.body hosts is equal to the JSON post sent from the user
+//   // This works because of our body parsing middleware
+//   var newCharacter = req.body;
+
+//   // Using a RegEx Pattern to remove spaces from newCharacter
+//   // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+//   newCharacter.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
+
+//   console.log(newCharacter);
+
+//   characters.push(newCharacter);
+
+//   res.json(newCharacter);
+// });
+
+
+
 
 // Starts the server to begin listening
 // =============================================================
